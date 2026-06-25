397eac987f3bec88ee46ff0a5fa37010decbb10b
williamtuominiemi <william.m.tuominiemi@gmail.com>
2026-06-25 21:15:02 +0300

    set up egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_ui_native("Orbit propagator", options, move |ui, _frame| {
+        egui::CentralPanel::default().show_inside(ui, |ui| {
+            ui.heading("Orbit propagator");
+        });
+    })
 }
